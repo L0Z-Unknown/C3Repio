faadea23027fbd678168591b2606371d434e8f08
L0Z-Unknown <sheadan34@gmail.com>
2024-10-23 11:54:42 -0700

    Update Math.cs

--- My project/Assets/Scenes/Scripts/Math.cs
+++ My project/Assets/Scenes/Scripts/Math.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Math : MonoBehaviour
 {
+    [SerializeField]
+    TMP_InputField markup;
+    [SerializeField]
+    TMP_InputField Price;
     // Start is called before the first frame update
     void Start()
     {
